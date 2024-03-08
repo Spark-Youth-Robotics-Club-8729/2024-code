482ed734e33fb1728d609ce92c6395b0f05fa0fd
DerekY2 <derek.yu869@gmail.com>
2024-03-08 15:35:02 -0500

    updated autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC2024GOOD";
+  public static final String MAVEN_NAME = "2024-code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "c3d67bf2c927e98f63ae13c7215e741154353013";
-  public static final String GIT_DATE = "2023-08-26 14:31:29 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-08 01:35:57 EST";
-  public static final long BUILD_UNIX_TIME = 1709879757873L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "9add1519c9d8440b734617f5b431321a219e77d4";
+  public static final String GIT_DATE = "2024-03-08 02:14:12 EST";
+  public static final String GIT_BRANCH = "newmarket";
+  public static final String BUILD_DATE = "2024-03-08 08:35:44 EST";
+  public static final long BUILD_UNIX_TIME = 1709904944755L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
